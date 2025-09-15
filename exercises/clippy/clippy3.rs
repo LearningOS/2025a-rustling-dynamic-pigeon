f01e24c9cef21fce88df178704997bcd43008da2
Dynamic_Pigeon <3040313336@qq.com>
2025-09-15 11:31:46 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,6 +4,7 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
+
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
@@ -14,8 +15,8 @@ fn main() {
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let mut my_vec = vec![1, 2, 3, 4, 5];
-    println!("This Vec is empty, see? {:?}", my_vec);
+    let my_empty_vec: Vec<i32> = vec![];
+    println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
